0bd82819f1857ae782112924d1c54fc44781cddd
ridhorayputraa <ridhorayputra033@Gmail.com>
2023-02-18 15:56:29 +0700

    Event Listener

--- src/components/Posting.tsx
+++ src/components/Posting.tsx
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 type dataProps = {
-  id: number
-  userId: number
-  title: string
-  body: string
-  toggle: 'Open' | 'Closed'
-}
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+  toggle: "Open" | "Closed";
+};
 // karena dia hanya menerima maka begini saja sudah cukup
 
 function Posting({ id, userId, title, body }: dataProps) {
+  const [newTitle, setNewTitle] = useState<string>("");
+
+  function buttonClick(e:MouseEvent) {
+    setNewTitle("wooo");
+    // untuk event listener tambahkan types event di dalam parameter nya
+  }
+
   return (
     <div>
+      <h1>{newTitle}</h1>
+      <button onClick={buttonClick}>Click me</button>
       <p>id: {id}</p>
       <p>title: {title}</p>
       <p>body: {body}</p>
